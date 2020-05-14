7dcfe7b0d17593ffc8212c3690ecfb4946cc2c14
ckipp01 <ckipp@pm.me>
2020-05-14 15:52:22 +0200

    Add in long method for testing

--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -1,9 +1,20 @@
 package example
 
 object Hello extends Greeting with App {
+  val x = superLongMethodToTestForHover("a", "b", "c", "d", "e", "f", "g")
   println(greeting)
 }
 
 trait Greeting {
   lazy val greeting: String = "hello"
+
+  def superLongMethodToTestForHover(
+      a: String,
+      b: String,
+      c: String,
+      d: String,
+      e: String,
+      f: String,
+      g: String
+  ) = ()
 }
